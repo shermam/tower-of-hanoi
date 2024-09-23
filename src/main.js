aa24b9f6adb0829d4d14496452d295a0eebcc33b
Shermam Miranda <shermam.miranda@hotmail.com>
2024-09-23 02:34:04 +0000

    Implement first version of render

--- src/main.js
+++ src/main.js
@@ -1,27 +1,43 @@
 class Hanoi {
   state = [[8, 7, 6, 5, 4, 3, 2, 1], [], []];
 
-  run() {
+  constructor(render) {
+    this.render = render;
+  }
+
+  async run() {
     const from = 0; // First tower
     const to = 1; // Second tower
     const aux = 2; // Third tower
-    this.move(this.state[from].length, from, to, aux);
+    await this.move(this.state[from].length, from, to, aux);
   }
 
-  move(n, from, to, aux) {
+  async move(n, from, to, aux) {
     if (n === 0) {
       return;
     } else {
-      this.move(n - 1, from, aux, to);
-      this.makeMove(from, to);
-      this.move(n - 1, aux, to, from);
+      await this.move(n - 1, from, aux, to);
+      await this.makeMove(from, to);
+      await this.move(n - 1, aux, to, from);
     }
   }
 
-  makeMove(from, to) {
+  async makeMove(from, to) {
     this.state[to].push(this.state[from].pop() ?? 0);
+    await this.render(this);
   }
 }
 
-const hanoi = new Hanoi();
-hanoi.run();
+(async () => {
+  async function render(hanoi) {
+    const hanoiDiv = document.querySelector("div#hanoi");
+    if (!hanoiDiv) throw new Error("Div not found");
+    hanoiDiv.innerHTML = JSON.stringify(hanoi.state, null, 2);
+    await new Promise((resolve) => {
+      document.addEventListener("keydown", resolve);
+    });
+  }
+  const hanoi = new Hanoi(render);
+  await render(hanoi);
+  hanoi.run();
+})();
