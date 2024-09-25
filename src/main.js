232b70bf2a4798a7bcdd8dae8e07da383ca72fd8
Shermam Miranda <shermam.miranda@hotmail.com>
2024-09-25 02:49:56 +0000

    Add move count

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 class Hanoi {
   state = [[8, 7, 6, 5, 4, 3, 2, 1], [], []];
+  moveCount = 0;
 
   constructor(render) {
     this.render = render;
@@ -13,26 +14,24 @@ class Hanoi {
   }
 
   async move(n, from, to, aux) {
-    if (n === 0) {
-      return;
-    } else {
-      await this.move(n - 1, from, aux, to);
-      await this.makeMove(from, to);
-      await this.move(n - 1, aux, to, from);
-    }
+    if (n === 0) return;
+    await this.move(n - 1, from, aux, to);
+    await this.makeMove(from, to);
+    await this.move(n - 1, aux, to, from);
   }
 
   async makeMove(from, to) {
     this.state[to].push(this.state[from].pop() ?? 0);
+    this.moveCount++;
     await this.render(this);
   }
 }
 
 (async () => {
   async function render(hanoi) {
-    const hanoiDiv = document.querySelector("div#hanoi");
-    if (!hanoiDiv) throw new Error("Div not found");
-    hanoiDiv.innerHTML = JSON.stringify(hanoi.state, null, 2);
+    const hanoiElement = document.querySelector("#hanoi");
+    if (!hanoiElement) throw new Error("Element not found");
+    hanoiElement.innerHTML = JSON.stringify(hanoi, null, 2);
     await new Promise((resolve) => {
       document.addEventListener("keydown", resolve);
     });
