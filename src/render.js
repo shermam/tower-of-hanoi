2d14ee8f7139144d37b1ef8886ef629e5e579752
Shermam Miranda <shermam.miranda@hotmail.com>
2024-09-25 03:32:18 +0000

    Add colors to the discs and move tower the right way up

--- src/render.js
+++ src/render.js
@@ -14,19 +14,32 @@ export async function render(hanoi) {
   const discCount = hanoi.state.flat().length;
   const discHeight = canvas.height / discCount;
   const maxDiscWidth = canvas.width / hanoi.state.length;
+
+  const colors = new Map();
+  for (let i = 1; i <= discCount; i++) {
+    const red = Math.floor((255 / discCount) * i);
+    const green = Math.floor(255 - (255 / discCount) * i);
+    colors.set(
+      i,
+      `#${red.toString(16).padStart(2, "0")}${green
+        .toString(16)
+        .padStart(2, "0")}00`
+    );
+  }
+
   for (let i = 0; i < hanoi.state.length; i++) {
     const tower = hanoi.state[i];
     for (let j = 0; j < tower.length; j++) {
       const discNumber = tower[j];
-      ctx.rect(
+      ctx.fillStyle = colors.get(discNumber);
+      ctx.fillRect(
         maxDiscWidth * i,
-        j * discHeight,
+        canvas.height - (j + 1) * discHeight,
         (maxDiscWidth / discCount) * discNumber,
         discHeight
       );
     }
   }
-  ctx.fill();
   ctx.closePath();
 
   await new Promise((resolve) => {
