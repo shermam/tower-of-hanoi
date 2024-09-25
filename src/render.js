019efff0e5199a7a7f3d29f64cd3d779f51d22e6
Shermam Miranda <shermam.miranda@hotmail.com>
2024-09-25 03:21:50 +0000

    Render discs in a canvas

--- src/render.js
+++ src/render.js
@@ -1,7 +1,34 @@
+const moveCountElement = document.querySelector("#move-count");
+const container = document.querySelector("div#canvas-container");
+const canvas = document.createElement("canvas");
+container?.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
 export async function render(hanoi) {
-  const hanoiElement = document.querySelector("#hanoi");
-  if (!hanoiElement) throw new Error("Element not found");
-  hanoiElement.innerHTML = JSON.stringify(hanoi, null, 2);
+  if (!moveCountElement) return;
+  if (!ctx) return;
+
+  moveCountElement.innerHTML = hanoi.moveCount;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  const discCount = hanoi.state.flat().length;
+  const discHeight = canvas.height / discCount;
+  const maxDiscWidth = canvas.width / hanoi.state.length;
+  for (let i = 0; i < hanoi.state.length; i++) {
+    const tower = hanoi.state[i];
+    for (let j = 0; j < tower.length; j++) {
+      const discNumber = tower[j];
+      ctx.rect(
+        maxDiscWidth * i,
+        j * discHeight,
+        (maxDiscWidth / discCount) * discNumber,
+        discHeight
+      );
+    }
+  }
+  ctx.fill();
+  ctx.closePath();
+
   await new Promise((resolve) => {
     document.addEventListener("keydown", resolve);
   });
